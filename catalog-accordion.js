ca99d300ea64e3757a108a2314a475cdd653f989
orozmamatovaelnura <orozmamatovaelnura74@gmail.com>
2023-07-06 11:01:02 +0600

    first commit

---
+++ catalog-accordion.js
@@ -0,0 +1,30 @@
+/*
+var w = window.innerWidth;
+var acc = document.getElementsByClassName("accordion");
+
+if (w < 770 || w > 420) {
+    var panel = acc.nextElementSibling;
+    panel.style.display = "block";
+}
+
+*/
+
+
+
+
+/*
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+*/
